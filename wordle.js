92c22c083d5f857af8b737408b491426cf22fbe0
Dudoop <136383947+Duduoop@users.noreply.github.com>
2024-03-30 21:13:46 +0100

    Add files via upload

---
+++ wordle.js
@@ -0,0 +1,89 @@
+var height = 6; //number of uesses
+var width = 5; //length of the word
+
+var row = 0;//current guess (attempt #)
+var col = 0;//current letter for that attampt
+
+var gameOver = false;
+var word = "SQUID";
+
+
+
+window.onload = function() {
+    intialize();
+}
+
+function intialize() {
+    //create the board
+    for (let r = 0; r < height; r++) {
+        for (let c = 0; c < width; c++) {
+            // <span id="0-0" class="tile"></span>
+            let tile = document.createElement("span");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            tile.innerText = "";
+            document.getElementById("board").appendChild(tile);
+        }
+    }
+    
+    //Listen for Key press
+    document.addEventListener("keyup", (e) => {
+        if (gameOver) return;
+        
+//        alert(e.code);
+    if ("KeyA" <= e.code && e.code <= "KeyZ") {
+        if (col < width) {
+            let currTile = document.getElementById(row.toString() + '-' + col.toString());
+            if (currTile.innerText == "") {
+                currTile.innerText = e.code[3];
+                col += 1;
+            }
+        }
+    }
+    else if (e.code == "Backspace") {
+        if (0 < col && col < width) {
+            col -=1;
+        }
+        let currTile = document.getElementById(row.toString() + '-' + col.toString());
+        currTile.innerText = "";
+    }
+    
+    else if (e.code == "Enter") {
+        update();
+        row += 1;//start new row to write om
+        col = 0;//strat at 0 for new row
+    }
+    
+    if(!gameOver && row == height) {
+        gameOver = true;
+        document.getElementById("answer").innerText = word;
+    }
+    
+  })
+  
+}
+
+function update() {
+    let correct = 0;
+    for (let c = 0; c < width; c++) {
+        let currTile = document.getElementById(row.toString() + '-' + c.toString());
+        let letter = currTile.innerText;
+        
+        //Is it in the correct position?
+        if (word[c] == letter) {
+            currTile.classList.add("correct");
+            correct += 1;
+            //Is it in the word?
+        }
+        else if (word.includes(letter)) {
+            currTile.classList.add("present");
+        } //Not in the word?
+        else {
+            currTile.classList.add("absent");
+        }
+        
+        if (correct == width) {
+            gameOver = true;
+        }
+    }
+}
